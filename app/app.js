10bbc40869556b41c0c7f7bab9f23d8e21c8b22d
Nathan Dunn <ndunn@me.com>
2015-10-10 14:43:14 -0700

    updated changes

--- app/app.js
+++ app/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var jokesArray = [
+  {id:1,question:'What is a catepillar afraid of?',answer:'A dogapillar!'}
+  ,{id:2,question:'Finance + toys = ? ',answer:'Doll-ar'}
+  ,{id:3,question:'What is a part of the body + insect = ? ',answer:'Butt-erfly'}
+];
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
@@ -7,6 +13,21 @@ angular.module('myApp', [
   'myApp.view2',
   'myApp.version'
 ]).
+controller('MainCtrl', function($scope,$http,$attrs) {
+
+      $scope.abc = 0 ;
+      //$scope.jokequestion
+      //$scope.jokeanswer = '';
+
+  $scope.load = function(){
+    $scope.jokes = jokesArray ;
+  };
+
+  $scope.load();
+
+}).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+
